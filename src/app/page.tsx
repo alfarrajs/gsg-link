fb3b5254a7e9ef9e96323c443165d0b4aa337977
Hijazimohammed <30778440+Hijazimohammed@users.noreply.github.com>
2023-09-21 23:19:16 +0300

    clone finished

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import LoginPage from '@/components/pages/LoginPage'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main >
+      <LoginPage />
+    </main>
+  )
+}
